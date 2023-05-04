d80e947cfffb94ed30d1639eabd57b7a980d098c
Designer225 <designer2553@gmail.com>
2023-05-03 22:35:58 -0700

    Forgot to add ignore behavior to patcher

--- Source/RemoveDevStageLimitPatcher.cs
+++ Source/RemoveDevStageLimitPatcher.cs
@@ -13,8 +13,10 @@ namespace RemoveDevStageLimit
     {
         static RemoveDevStageLimitPatcher()
         {
-            foreach (var apparel in DefDatabase<ThingDef>.AllDefs.Where(x => x.IsApparel).Select(x => x.apparel))
+            foreach (var def in DefDatabase<ThingDef>.AllDefs.Where(x => x.IsApparel))
             {
+                if (RemoveDevStageLimitSettings.Instance.IgnoredApparels.TryGetValue(def, out bool value) && value) continue;
+                var apparel = def.apparel;
                 var filterToAppend = DevelopmentalStage.None;
                 if ((apparel.developmentalStageFilter & DevelopmentalStage.Adult) == DevelopmentalStage.Adult
                     && RemoveDevStageLimitSettings.Instance.MakeAdultApparelUsable)
@@ -23,6 +25,8 @@ namespace RemoveDevStageLimit
                     && RemoveDevStageLimitSettings.Instance.MakeChildApparelUsable)
                     filterToAppend |= DevelopmentalStage.Adult;
                 apparel.developmentalStageFilter |= filterToAppend;
+                if (RemoveDevStageLimitSettings.Instance.Debug)
+                    Log.Message("[RemoveDevStageLimit] Patched " + def.defName.ToString());
             }
         }
     }
